7b3cf09e68fe30e30e8b513d5ea6a57d897dcd1f
Cole Ellison <cole.ellison@gmail.com>
2016-06-30 10:57:02 -0400

    passport stuff

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
+var fID = process.env.FACEBOOK_ID
+var fSecret = process.env.FACEBOOK_SECRET
+
+var auth = require('./routes/auth');
 var routes = require('./routes/index');
+var models = require('./models/models')
 var users = require('./routes/users');
 
 var app = express();
@@ -22,8 +30,47 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+  // {passReqToCallback : true},
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      console.log('find user called')
+      if (err) { return done(err);}//  done(err);
+      if (!user) {
+        return done(null, false,{ message: 'Incorrect username.' });
+      }
+      if (!user.validatePassword(password)) {
+        return done(null, false,{ message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: fID,
+    clientSecret: fSecret,
+    callbackURL: "localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile)
+    User.findOrCreate({ facebookID: profile.id, username:profile.displayName }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', auth(passport));
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
